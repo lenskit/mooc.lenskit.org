35a8cbbc938f7d57e47a4aac89c6b508f8db50a2
Michael Ekstrand <md@ekstrandom.net>
2016-07-23 12:39:56 -0600

    Remove unnecessary print

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,6 @@ const gulp = require('gulp');
 
 gulp.task('manual', function(callback) {
   const gitbook = require('gitbook');
-  gitbook.commands.forEach((cmd) => console.log(cmd.name))
   let command = gitbook.commands.find((cmd) => cmd.name.match(/^build /));
   if (command === undefined) {
     return callback(new Error("cannot find gitbook 'build' command"))
