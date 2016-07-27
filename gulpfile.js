e3f500b2df7bbb3d41c9dcdfc26a57d820bbedce
Michael Ekstrand <md@ekstrandom.net>
2016-07-27 09:14:25 -0500

    Copy API docs with Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,10 @@
 "use strict";
 const gulp = require('gulp');
+const g = {
+  changed: require('gulp-changed')
+};
+
+const outputDir = '_site';
 
 gulp.task('manual', function(callback) {
   const gitbook = require('gitbook');
@@ -10,3 +15,9 @@ gulp.task('manual', function(callback) {
   return command.exec([__dirname, 'documentation'],
                       {log: 'info', format: 'website'});
 });
+
+gulp.task('copy-docs', function() {
+  return gulp.src('static/**', {buffer: false})
+             .pipe(g.changed(outputDir))
+             .pipe(gulp.dest(outputDir));
+})
